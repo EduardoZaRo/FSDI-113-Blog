79ba84ff3cdd82f8e19b09373084171fdcdc7157
EduardoZaRo <irvin.zavala@uabc.edu.mx>
2023-10-30 13:08:41 -0700

    Class 2 Notes

---
+++ README.md
@@ -0,0 +1,7 @@
+# Intermediate Django | SDGKU
+
+## Continuation of FSDI-112 Blog system: https://ancient-temple-14710-3fec06e124ce.herokuapp.com
+
+Write here important notes about the project 
+
+//Silence is golden
---
+++ posts/views.py
@@ -0,0 +1,101 @@
+from django.shortcuts import render
+from django.views.generic import(
+    ListView, 
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from .models import Post, Status
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin,
+)
+from django.core.exceptions import PermissionDenied
+
+class PostListView(ListView):
+    template_name = "posts/list.html"
+    model = Post
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        published_status = Status.objects.get(name="published")
+        context["post_list"] = Post.objects.filter(
+            status=published_status
+        ).order_by("created_on").reverse()
+        return context
+
+class DraftPostListView(ListView):
+    template_name = "posts/list.html"
+    model = Post
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        draft_status = Status.objects.get(name="draft")
+        context["post_list"] = Post.objects.filter(
+            status=draft_status
+        ).filter(
+            author=self.request.user
+        ).order_by("created_on").reverse()
+        return context
+
+class ArchivedPostListView(LoginRequiredMixin, ListView):
+    template_name = "posts/list.html"
+    model = Post
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # if (
+        #     self.request.user.is_anonymous
+        #     ):
+        #     raise PermissionDenied()
+        archived_status = Status.objects.get(name="archived")
+        context["post_list"] = Post.objects.filter(
+            status=archived_status
+        ).order_by("created_on").reverse()
+        return context
+    
+class PostDetailtView(DetailView):
+    template_name = "posts/detail.html"
+    model = Post
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if (
+            context["post"].status.name == "draft" and 
+            context["post"].author != self.request.user 
+            ):
+            raise PermissionDenied()
+        if (
+            context["post"].status.name == "archived" and 
+            self.request.user.is_anonymous
+            ):
+            raise PermissionDenied()
+        return context
+    
+class PostCreateView(LoginRequiredMixin, CreateView):
+    template_name = "posts/new.html"
+    model = Post
+    fields = ["title","subtitle","body","status"]
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    template_name = "posts/edit.html"
+    model = Post
+    fields = ["title","subtitle","body","status"]
+    
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+    
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    template_name = "posts/delete.html"
+    model = Post
+    success_url = reverse_lazy("list")
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
